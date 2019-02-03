b9cc1462ff917a2ef3a9b446dac7c3c126187fef
ki-cooley <46787270+ki-cooley@users.noreply.github.com>
2019-02-02 20:37:56 -0500

    Add files via upload

---
+++ cal_to_json.py
@@ -0,0 +1,25 @@
+import pickle
+import json
+
+with open('events.pkl', 'rb') as f:
+    events = pickle.load(f)
+
+print(type(events[0]['start'].get('dateTime')))
+
+mastercal = {"events":[]}
+for event in events:
+    masterevent = {
+            "title": event['id'],
+            "start":event['start'].get('dateTime', event['start'].get('date')),
+            "end":event['end'].get('dateTime', event['end'].get('date')),
+            #super hacky but oh well--needs fixing later
+            "allDay?": event['start'].get('dateTime') is None,
+            "resource?": None
+    }
+
+    mastercal["events"].append(masterevent)
+
+print(mastercal["events"][1])
+#print(mastercal["events"][1])
+with open('mstcal.json', 'a') as outfile:  
+    json.dump(dict(mastercal), outfile)
