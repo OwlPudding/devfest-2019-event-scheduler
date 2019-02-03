df1cbcfaa4eaf1fb7de366a3cf5613be257a55e8
ki-cooley <46787270+ki-cooley@users.noreply.github.com>
2019-02-03 06:26:43 -0500

    Add files via upload

--- backend/cal_to_json.py
+++ backend/cal_to_json.py
@@ -1,38 +1,51 @@
 import pickle
 import json
+from random import randint
 
 
-def merge(old, new):
+def cal_to_json():
+    
+    def merge(old, new):
     new['events'] = old['events'] + new['events']
     return new
 
 
-with open('events.pkl', 'rb') as f:
-    events = pickle.load(f)
+    with open('events.pkl', 'rb') as f:
+        events = pickle.load(f)
 
-#print(type(events[1]['start'].get('dateTime')))
+    #print(type(events[1]['start'].get('dateTime')))
 
-mastercal = {"events":[]}
-for event in events:
-    masterevent = {
-            "title": event['id'],
-            "start":event['start'].get('dateTime', event['start'].get('date')),
-            "end":event['end'].get('dateTime', event['end'].get('date')),
-            #super hacky but oh well--needs fixing later
-            "allDay?": event['start'].get('dateTime') is None,
-            "resource?": None
-    }
+    uniqueUserId = randint(0, 9)
 
-    mastercal["events"].append(masterevent)
+    mastercal = {"events":[]}
+    for event in events:
+        masterevent = {
+                "title": uniqueUserId,
+                "start":event['start'].get('dateTime', event['start'].get('date')),
+                "end":event['end'].get('dateTime', event['end'].get('date')),
+                #super hacky but oh well--needs fixing later
+                "allDay?": event['start'].get('dateTime') is None,
+                "resource?": None
+        }
 
-#print(mastercal["events"][1])
+        mastercal["events"].append(masterevent)
 
-with open('mstcal.json', 'rb') as read_file:
-    old_json = json.loads(read_file.read())# read json
+    #print(mastercal["events"][1])
 
-merged_json = merge(old_json, mastercal)
+    import os
 
-with open('mstcal.json', 'w') as outfile:  
-    #merge and write json
-    json.dump(merged_json, outfile)
+    if s.stat("mstcal.json").st_size == 0:
+        with open('mstcal.json', 'w') as outfile:  
+            #merge and write json
+            json.dump(mstcal.json, outfile)
+
+    else:
+        with open('mstcal.json', 'rb') as read_file:
+            old_json = json.loads(read_file.read())# read json
+
+        merged_json = merge(old_json, mastercal)
+
+        with open('mstcal.json', 'w') as outfile:  
+            #merge and write json
+            json.dump(merged_json, outfile)
 
